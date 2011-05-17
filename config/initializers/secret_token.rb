ca66aa71088972794a4774c779f87e7dfd59e066
German Garcia <german.garcia.d@gmail.com>
2011-05-17 11:13:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epaymentplans::Application.config.secret_token = '6c03ef0b0d01426793ebb8b54a412dc0cc4fe54e220e690843cdd07e9271e4ac9f85c2938541ebfcabbb7361a8028fe0683bbe08d2c50cdf6fbd96107f584932'
