f3222c94926639d2f3e01010c2e03ab43c5e0ef5
roberto miranda <rjmaltamar@gmail.com>
2011-05-27 20:24:28 -0500

    fix notify http reponse after save the order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,13 @@ class Order < ActiveRecord::Base
   after_save :notify_store
   
   def notify_store
-    request = Net::HTTP::Post.new(notify_url)
+    url = URI.parse(notify_url)
+    request = Net::HTTP::Post.new(url.path)
     request['Content-Type'] = "application/x-www-form-urlencoded" 
+    request.set_form_data({:security_key=>"akjsndk777777", :transaction_id => 123444,
+                                        :order_id => num , :received_at => created_at, 
+                                        :status => "completed", :test => 'test'}, ';')
 
-    http = Net::HTTP.new(notify_url)
-    response = http.request(request, {:security_key=>"akjsndk777777", :transaction_id => 123444,
-                                      :order_id => num , :received_at => created_at, 
-                                      :status => "completed", :test => 'test'})
+    response = Net::HTTP.new(url.host, url.port).start {|http| http.request(request) }
   end
 end
