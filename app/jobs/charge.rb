2d741b5b85d158d00a7bb78a53f0a3dc8cf13679
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-07-05 13:34:15 -0500

    Output messages for job

--- app/jobs/charge.rb
+++ app/jobs/charge.rb
@@ -2,8 +2,12 @@ class Charge
   @queue = :charge
 
   def self.perform(payment_plan_id)
+    puts "Performing job.."
     plan = PaymentPlan.find(payment_plan_id)
-    plan.make_payment
+    puts "Payment Plan: #{plan.inspect}"
+    payment = plan.make_payment
+    puts "Payment Result: #{payment.inspect}"
+    puts "Job performed."
   end
 end
 
