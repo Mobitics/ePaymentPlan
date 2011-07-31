f90c338fad21b515320cc79bf539d756ffdac2c6
Jose <jose@jose-Inspiron-N4030.(none)>
2011-07-31 12:00:41 -0500

    As a store, I want to see the 5 newest orders (payment_plans) id=16434881

--- app/controllers/store/stores_controller.rb
+++ app/controllers/store/stores_controller.rb
@@ -2,5 +2,6 @@ class Store::StoresController < ApplicationController
   before_filter :authorized_store
   layout "store"
   def index
+  	@payment_plans=@store.payment_plans.order("created_at DESC").limit(5)
   end
 end
