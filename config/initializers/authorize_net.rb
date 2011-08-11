73899d0789dfb3599dec85dae81deb6d58804ba2
German Garcia <german.garcia.d@gmail.com>
2011-08-11 02:29:24 -0500

    Debugging 5

--- config/initializers/authorize_net.rb
+++ config/initializers/authorize_net.rb
@@ -1,15 +1,15 @@
 AuthorizeConfig = YAML.load_file(File.open("#{Rails.root}/config/stores.yml"))
 
-# ActiveMerchant::Billing::Base.mode = :test # unless Rails.env.production?
+ActiveMerchant::Billing::Base.mode = :test # unless Rails.env.production?
 
 module ActiveMerchant
   module Utils
-    def get_payment_gateway(api_key=nil, secret=nil, test=true)
+    def get_payment_gateway(api_key=nil, secret=nil, test_mode=false)
       gateway = ActiveMerchant::Billing::AuthorizeNetCimGateway.new(
         :login => api_key || AuthorizeConfig['Store1']['authorizenet']['api_key'],
         :password => secret || AuthorizeConfig['Store1']['authorizenet']['secret'],
-        :test => test
-        )
+        :test => test_mode
+      )
       if not gateway
         raise AuthenticationFailed, 'Authentication with CIM Gateway could not be completed.'
       end
